1c3c1e705c94498e32b51e4a82b3e94136d1b572
Marius Rochon <mrochon@microsoft.com>
2020-03-01 17:14:30 -0800

    Adding DisplayClaims

---
+++ Policies/DisplayControls/Upload.ps1
@@ -0,0 +1,5 @@
+﻿# connect-azuread -tenantid mrochonb2cprod.onmicrosoft.com
+$source = 'C:\Users\mrochon\source\repos\b2csamples\Policies\DisplayControls'
+$conf = 'C:\Users\mrochon\source\repos\b2csamples\Policies\DisplayControls\conf.json'
+$dest = 'C:\Users\mrochon\source\repos\b2csamples\Policies\DisplayControls\uploaded'
+Upload-IEFPolicies -sourceDirectory $source -configurationFilePath $conf -updatedSourceDirectory $dest -prefix 'DC' 
