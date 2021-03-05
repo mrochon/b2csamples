5f0f38749b9f73d29439d86f4887d3f2703c460a
Marius Rochon <mrochon@microsoft.com>
2021-03-04 20:19:06 -0800

    Added sample client app to use id_token_hint policies

---
+++ Apps/IdTokenHint/IdTokenHint/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace IdTokenHint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var p = new Program();
+            var id = p.SignInAsync().Result;
+            id = p.EditProfileAsync(id).Result;
+        }
+        private static string Tenant = "mrochonb2cprod";
+        private static string AuthBase = $"https://{Tenant}.b2clogin.com/tfp/{Tenant}.onmicrosoft.com/";
+        private static string ClientId = "75bb114c-bb4d-4134-8fc6-1f3aa84b0759";
+
+        private async Task<string> SignInAsync()
+        {
+            var client = PublicClientApplicationBuilder
+                .Create(ClientId)
+                .WithB2CAuthority($"{AuthBase}B2C_1A_IDHINTsignup_signin")
+                .WithRedirectUri("http://localhost")
+                .Build();
+            var tokens = await client.AcquireTokenInteractive(new string[] { }).ExecuteAsync();
+            return tokens.IdToken;
+        }
+        private async Task<string> EditProfileAsync(string id_token)
+        {
+            var client = PublicClientApplicationBuilder
+                .Create(ClientId)
+                .WithB2CAuthority($"{AuthBase}B2C_1A_IDHINTProfileEdit")
+                .WithRedirectUri("http://localhost")
+                .WithExtraQueryParameters($"id_token_hint={id_token}")
+                .Build();
+            var tokens = await client.AcquireTokenInteractive(new string[] { }).ExecuteAsync();
+            return tokens.IdToken;
+        }
+    }
+}
--- Apps/Readme.md
+++ Apps/Readme.md
@@ -1,1 +1,9 @@
+# Apps used in various samples
+
+## B2CMultiTenant
 This project has been moved to [a separate repo](https://github.com/mrochon/b2c-mt-webapp) to enable use of [scripts](https://github.com/mrochon/b2csamples/tree/master/Scripts/MultiTenant) for deploying it to any Azure subscription.
+
+## IdTokenHint
+A .NET Core Console app using MSAL to initiate the ProfileEdit journey
+using id_token returned from b2C signin journey. See 
+[corresponding xml policies](https://github.com/mrochon/b2csamples/tree/master/Policies/IdTokenSelfHint).
