fad8e2256557c0051a814ebbf60c6b039be2f5b4
Marius Rochon <mrochon@microsoft.com>
2021-03-04 20:28:57 -0800

    Added refresh step to id token hint client

--- Apps/IdTokenHint/IdTokenHint/Program.cs
+++ Apps/IdTokenHint/IdTokenHint/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Identity.Client;
 using System;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace IdTokenHint
 {
@@ -11,20 +12,29 @@ namespace IdTokenHint
             Console.WriteLine("Hello World!");
             var p = new Program();
             var id = p.SignInAsync().Result;
+            id = p.Refresh().Result;
             id = p.EditProfileAsync(id).Result;
         }
         private static string Tenant = "mrochonb2cprod";
         private static string AuthBase = $"https://{Tenant}.b2clogin.com/tfp/{Tenant}.onmicrosoft.com/";
         private static string ClientId = "75bb114c-bb4d-4134-8fc6-1f3aa84b0759";
+        private static IPublicClientApplication Client;
 
         private async Task<string> SignInAsync()
         {
-            var client = PublicClientApplicationBuilder
+            Client = PublicClientApplicationBuilder
                 .Create(ClientId)
                 .WithB2CAuthority($"{AuthBase}B2C_1A_IDHINTsignup_signin")
                 .WithRedirectUri("http://localhost")
                 .Build();
-            var tokens = await client.AcquireTokenInteractive(new string[] { }).ExecuteAsync();
+            var tokens = await Client.AcquireTokenInteractive(new string[] { }).ExecuteAsync();
+            return tokens.IdToken;
+        }
+        private async Task<string> Refresh()
+        {
+            var accounts = await Client.GetAccountsAsync();
+            var account = accounts.First();
+            var tokens = await Client.AcquireTokenSilent(new string[] { "openid" }, account).ExecuteAsync();
             return tokens.IdToken;
         }
         private async Task<string> EditProfileAsync(string id_token)
