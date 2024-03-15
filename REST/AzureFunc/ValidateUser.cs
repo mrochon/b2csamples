7d8e26a31860ba389f4eec0ab9dc17cb8355aec9
MariusR <mrochon@microsoft.com>
2024-03-15 14:54:31 -0700

    Added ValidateUser API

---
+++ REST/AzureFunc/ValidateUser.cs
@@ -0,0 +1,40 @@
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace MR.AzureFunc
+{
+    public class ValidateUser
+    {
+        private readonly ILogger<ValidateUser> _logger;
+
+        public ValidateUser(ILogger<ValidateUser> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("ValidateUser")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("ValidateUser starting.");
+            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            var user = JsonSerializer.Deserialize<User>(body);
+            if((user == null) || String.IsNullOrEmpty(user.displayName) || String.IsNullOrEmpty(user.extension_9fa255f4f18847feb0c166c6f2ad769a_EmployeeId))
+                return new BadRequestObjectResult(new { version = "1.0.0", status = 409, userMessage = "Invalid or missing user data." });
+            if(user.displayName.EndsWith(user.extension_9fa255f4f18847feb0c166c6f2ad769a_EmployeeId))
+                return new OkObjectResult(new { version = "1.0.0", status = 200, userMessage = "User validated" });
+            return new BadRequestObjectResult(new { version = "1.0.0", status = 409, userMessage = "Your signup data is invalid." });
+        }
+    }
+    public class User
+    {
+        public string? displayName { get; set; }
+        public string? givenName { get; set; }
+        public string? surname { get; set; }
+        public string? extension_9fa255f4f18847feb0c166c6f2ad769a_EmployeeId { get; set; }
+    }
+}
